6542633f39fe5ee890d76a45701b06ca4fddfada
176608 <al176608@alumnos.uacj.mx>
2025-04-09 11:10:07 -0600

    Add Envoy configuration for deployment and cache clearing tasks

---
+++ Envoy.blade.php
@@ -0,0 +1,34 @@
+@servers(['web' => 'envoy-server'])
+
+@setup
+    $branch = 'master';
+    $path = '/public_html/bev-bomberos';
+@endsetup
+
+@task('deploy', ['on' => 'web'])
+    cd {{ $path }}
+
+    echo "Descargando cambios..."
+    git pull origin {{ $branch }}
+
+    echo "Instalando dependencias..."
+    composer install --no-dev --optimize-autoloader
+
+    echo "Ejecutando migraciones..."
+    php artisan migrate --force
+
+    echo "Limpiando caché..."
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+
+    echo "Despliegue completado."
+@endtask
+
+@task('clear-cache', ['on' => 'web'])
+    cd {{ $path }}
+    php artisan cache:clear
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+@endtask
