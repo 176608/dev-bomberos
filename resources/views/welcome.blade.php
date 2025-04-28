cf7eac9253bf94e04722c7c8ecd608c909f26a82
176608 <al176608@alumnos.uacj.mx>
2025-04-28 13:59:20 -0600

    Update title in welcome.blade.php to "Modulo Bomberos"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>welcome_blade_php</title>
+        <title>Modulo Bomberos</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
