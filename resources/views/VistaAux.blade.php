8d778a47097ac9bbfb82cb904a8958145336c1de
176608 <al176608@alumnos.uacj.mx>
2025-05-08 11:33:24 -0600

    Remove auxiliary view layout and replace with phpinfo() for debugging purposes

--- resources/views/VistaAux.blade.php
+++ resources/views/VistaAux.blade.php
@@ -1,25 +1,3 @@
-@extends('layouts.app')
-
-@section('title', 'Vista Auxiliar')
-
-@section('content')
-<div class="card">
-    <div class="card-header">
-        <h2>Vista Auxiliar</h2>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table id="auxTable" class="table table-bordered table-striped">
-                <thead class="table-dark">
-                    <tr>
-                        <!-- Aquí irán los encabezados de tu tabla -->
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Aquí irá el contenido de tu tabla -->
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
-@endsection
+<?php
+phpinfo();
+?>
