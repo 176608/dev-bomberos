bba94d78a6063cac65ada0bb5b92d8cfa279c256
176608 <al176608@alumnos.uacj.mx>
2025-06-19 13:49:48 -0600

    limpiando codigo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,53 +3,13 @@
 @section('title', 'Hidrantes')
 
 @section('content')
-<!--Para agregar en la tabla es TH y TD-->
-<div class="card">
-    <div class="card-header">
-        <h2>Listado de Hidrantes</h2>
-    </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table id="hidrantesTable" class="table table-bordered table-striped">
-                <thead class="table-dark">
-                    <tr>
-                        <th>ID</th>
-                        <th>Fecha Alta</th>
-                        <th>Colonia</th>
-                        <th>Calle</th>
-                        <th>Y Calle</th>
-                        <th>Oficial</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($registros as $registro)
-                        <tr>
-                            <td>{{ $registro->id }}</td>
-                            <td>{{ $registro->fecha_inspeccion }}</td>
-                            <td>{{ $registro->colonia }}</td>
-                            <td>{{ $registro->calle }}</td>
-                            <td>{{ $registro->y_calle }}</td>
-                            <td>{{ $registro->oficial }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+
+LANDING PAGE
 
 @endsection
 
 @section('scripts')
 <script>
-    $(document).ready(function() {
-        $('#hidrantesTable').DataTable({
-            language: {
-                url: "{{ asset('js/datatables/i18n/es-ES.json') }}"
-            },
-            pageLength: 25,
-            order: [[0, 'desc']]
-        });
-    });
+
 </script>
 @endsection
