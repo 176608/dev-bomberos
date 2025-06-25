a186fcc2a77082d907c4033ed9f7a35089ed0788
176608 <al176608@alumnos.uacj.mx>
2025-06-25 13:19:24 -0600

    fix en edit

--- resources/views/partials/hidrante-form.blade.php
+++ resources/views/partials/hidrante-form.blade.php
@@ -49,7 +49,7 @@
                                 </div>
                                 <hr class="my-2">
                                 <div class="row">
-                                    <div class="col-md-6 mb-3 offset-md-2">
+                                    <div class="col-md-6 mb-3 offset-md-3">
                                         <label class="form-label">Número de Estación:</label>
                                         <select class="form-select" name="numero_estacion">
                                             <option value="" {{ empty($hidrante->numero_estacion) ? 'selected disabled' : '' }}>S/D</option>
