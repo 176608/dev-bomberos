8fc74b17f772039f90eb3875f81d8a8f7574e06b
176608 <al176608@alumnos.uacj.mx>
2025-07-14 13:01:00 -0600

    test new modal

--- resources/views/partials/hidrante-view.blade.php
+++ resources/views/partials/hidrante-view.blade.php
@@ -2,44 +2,113 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header bg-primary text-white">
-                <h5 class="modal-title">Reporte del Hidrante #{{ $hidrante->id }}</h5>
+                <h5 class="modal-title w-100 text-center">Departamento de Bomberos</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
             </div>
             <div class="modal-body">
-                <dl class="row">
-                    <dt class="col-sm-4">Estatus</dt>
-                    <dd class="col-sm-8">{{ $hidrante->stat }}</dd>
-                    <dt class="col-sm-4">Fecha Inspección</dt>
-                    <dd class="col-sm-8">{{ $hidrante->fecha_inspeccion }}</dd>
-                    <dt class="col-sm-4">N° Estación</dt>
-                    <dd class="col-sm-8">{{ $hidrante->numero_estacion }}</dd>
-                    <dt class="col-sm-4">Calle</dt>
-                    <dd class="col-sm-8">{{ $hidrante->callePrincipal?->Nomvial ?? $hidrante->calle }}</dd>
-                    <dt class="col-sm-4">Y Calle</dt>
-                    <dd class="col-sm-8">{{ $hidrante->calleSecundaria?->Nomvial ?? $hidrante->y_calle }}</dd>
-                    <dt class="col-sm-4">Colonia</dt>
-                    <dd class="col-sm-8">{{ $hidrante->coloniaLocacion?->NOMBRE ?? $hidrante->colonia }}</dd>
-                    <dt class="col-sm-4">Llave Hidrante</dt>
-                    <dd class="col-sm-8">{{ $hidrante->llave_hidrante }}</dd>
-                    <dt class="col-sm-4">Presión Agua</dt>
-                    <dd class="col-sm-8">{{ $hidrante->presion_agua }}</dd>
-                    <dt class="col-sm-4">Llave Fosa</dt>
-                    <dd class="col-sm-8">{{ $hidrante->llave_fosa }}</dd>
-                    <dt class="col-sm-4">Ubicación Fosa</dt>
-                    <dd class="col-sm-8">{{ $hidrante->ubicacion_fosa }}</dd>
-                    <dt class="col-sm-4">Conectado a Tubo</dt>
-                    <dd class="col-sm-8">{{ $hidrante->hidrante_conectado_tubo }}</dd>
-                    <dt class="col-sm-4">Estado</dt>
-                    <dd class="col-sm-8">{{ $hidrante->estado_hidrante }}</dd>
-                    <dt class="col-sm-4">Marca</dt>
-                    <dd class="col-sm-8">{{ $hidrante->marca }}</dd>
-                    <dt class="col-sm-4">Año</dt>
-                    <dd class="col-sm-8">{{ $hidrante->anio }}</dd>
-                    <dt class="col-sm-4">Oficial</dt>
-                    <dd class="col-sm-8">{{ $hidrante->oficial }}</dd>
-                    <dt class="col-sm-4">Observaciones</dt>
-                    <dd class="col-sm-8">{{ $hidrante->observaciones }}</dd>
-                </dl>
+                <h6 class="text-center mb-3">Registro de Hidrantes</h6>
+                <form>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Fecha de Inspección</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->fecha_inspeccion }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Fecha Tentativa de Mantenimiento</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->fecha_mantenimiento ?? '' }}" readonly>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">N° Estación</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->numero_estacion }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Número de Hidrante</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->id }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <h6 class="fw-bold">Ubicación</h6>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Calle</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->callePrincipal?->Nomvial ?? $hidrante->calle }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Y Calle</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->calleSecundaria?->Nomvial ?? $hidrante->y_calle }}" readonly>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Colonia</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->coloniaLocacion?->NOMBRE ?? $hidrante->colonia }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mb-2">
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Llave de Hidrante</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->llave_hidrante }}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Presión del Agua</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->presion_agua }}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Color</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->color ?? '' }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Ubicación de Fosa</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->ubicacion_fosa }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Llave de Fosa</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->llave_fosa }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mb-2">
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Hidrante Conectado a Tubo de</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->hidrante_conectado_tubo }}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Año</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->anio }}" readonly>
+                        </div>
+                        <div class="col-md-4">
+                            <label class="form-label fw-bold">Estado de Hidrante</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->estado_hidrante }}" readonly>
+                        </div>
+                    </div>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Marca</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->marca }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Observaciones</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->observaciones }}" readonly>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row mb-2">
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Atendido Maquina Num</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->maquina_num ?? '' }}" readonly>
+                        </div>
+                        <div class="col-md-6">
+                            <label class="form-label fw-bold">Oficial</label>
+                            <input type="text" class="form-control" value="{{ $hidrante->oficial }}" readonly>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
