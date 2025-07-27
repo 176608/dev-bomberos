10dd4f5eb581ce69c1739ccbe3c6975af3563b85
176608 <al176608@alumnos.uacj.mx>
2025-07-27 15:17:26 -0600

    Refactor y reroute

--- app/Http/Controllers/SIGEM/Controller.php
+++ app/Http/Controllers/SIGEM/Controller.php
@@ -1,13 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\SIGEM;
 /*
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-*/
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+*/
+
+abstract class Controller
+{
+    //
 }
