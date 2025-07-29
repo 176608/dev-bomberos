04ba777b303eb877e60d61c04dc1767aec4b49b6
176608 <al176608@alumnos.uacj.mx>
2025-07-29 11:32:14 -0600

    fix public

--- app/Http/Controllers/SIGEM/PublicController.php
+++ app/Http/Controllers/SIGEM/PublicController.php
@@ -2,7 +2,7 @@
 /* <!-- -RECIEN AGREGADO 25/07/2025- Archivo SIGEM - NO ELIMINAR COMENTARIO --> */
 namespace App\Http\Controllers\SIGEM;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\SIGEM\Controller;
 use Illuminate\Http\Request;
 use App\Models\SIGEM\Mapa;
 use App\Models\SIGEM\Tema;
