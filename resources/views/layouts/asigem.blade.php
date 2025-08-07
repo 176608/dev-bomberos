0488ddb5de18c2f175255f0e6482a1f4612a8e13
176608 <al176608@alumnos.uacj.mx>
2025-08-07 13:34:46 -0600

    test colores

--- resources/views/layouts/asigem.blade.php
+++ resources/views/layouts/asigem.blade.php
@@ -37,7 +37,7 @@
 
         /* Menú SIGEM */
         .main-menu {
-            background-color: #27a798ff !important;
+            background-color: #27a781ff !important;
             border-bottom: 3px solid #ffd700;
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
             width: 100%;
@@ -75,7 +75,7 @@
         }
 
         .main-menu a.active {
-            background-color: #0b584fff;
+            background-color: #165845ff ;
             color: #ffd700;
             font-weight: bold;
         }
