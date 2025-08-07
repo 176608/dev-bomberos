486bb918f9999ea75a4443ec00d8e77ab7bb3412
176608 <al176608@alumnos.uacj.mx>
2025-08-07 12:04:51 -0600

    fixxed estadistica

--- resources/views/partials/estadistica.blade.php
+++ resources/views/partials/estadistica.blade.php
@@ -7,14 +7,9 @@
         @if(isset($modo_vista) && $modo_vista === 'desde_catalogo')
 
         @elseif(isset($modo_vista) && $modo_vista === 'navegacion_tema_con_subtemas')
-            <!-- NUEVA VISTA: Incluir el partial específico para tema con subtemas -->
-             Entre en el if y cargo tema con subtemas
             @include('partials.estadistica_tema_con_subtemas')
             
-            
         @else
-            <!-- NUEVO ENFOQUE: Incluir la vista de navegación de temas -->
-             Entro aca?
             @include('partials.estadistica_navegacion')
         @endif  
     </div>
