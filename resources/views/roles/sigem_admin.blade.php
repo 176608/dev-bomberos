cbe799b8251414bfdcc3b0365a67a6e7abef0db7
176608 <al176608@alumnos.uacj.mx>
2025-08-08 02:31:19 -0600

    test

--- resources/views/roles/sigem_admin.blade.php
+++ resources/views/roles/sigem_admin.blade.php
@@ -1,10 +1,13 @@
 <!-- SIGEM PUNTO BLADE PUNTO PHP -->
 @extends('layouts.app')
-Estoy en sigem admin
+
 @section('admin_content')
+Estoy en section sigem admin
     <!-- Contenido inicial por defecto (se carga por JavaScript) -->
     <div class="Cargando">
         <i class="bi bi-hourglass-split"></i>
         <p>Cargando contenido...</p>
     </div>
 @endsection
+
+Estoy en sigem admin
