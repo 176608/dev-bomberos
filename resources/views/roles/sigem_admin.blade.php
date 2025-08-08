b6e7a66d73dc91c385d2d3bdc1a4c7416df5bce2
176608 <al176608@alumnos.uacj.mx>
2025-08-08 02:32:01 -0600

    clean sigem admin

--- resources/views/roles/sigem_admin.blade.php
+++ resources/views/roles/sigem_admin.blade.php
@@ -2,12 +2,9 @@
 @extends('layouts.app')
 
 @section('admin_content')
-Estoy en section sigem admin
     <!-- Contenido inicial por defecto (se carga por JavaScript) -->
     <div class="Cargando">
         <i class="bi bi-hourglass-split"></i>
         <p>Cargando contenido...</p>
     </div>
 @endsection
-
-Estoy en sigem admin
