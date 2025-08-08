426a38de36b036d46c09290944f68895dfb312bd
176608 <al176608@alumnos.uacj.mx>
2025-08-08 02:29:54 -0600

    Sigem admin

--- resources/views/roles/sigem_admin.blade.php
+++ resources/views/roles/sigem_admin.blade.php
@@ -1,7 +1,7 @@
 <!-- SIGEM PUNTO BLADE PUNTO PHP -->
-@extends('layouts.asigem')
+@extends('layouts.app')
 Estoy en sigem admin
-@section('dynamic_content')
+@section('admin_content')
     <!-- Contenido inicial por defecto (se carga por JavaScript) -->
     <div class="Cargando">
         <i class="bi bi-hourglass-split"></i>
