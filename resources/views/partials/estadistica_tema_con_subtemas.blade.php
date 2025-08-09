62d590fae16f166a25efd65a9834bcffd4b660a7
176608 <al176608@alumnos.uacj.mx>
2025-08-08 23:55:41 -0600

    1px fix

--- resources/views/partials/estadistica_tema_con_subtemas.blade.php
+++ resources/views/partials/estadistica_tema_con_subtemas.blade.php
@@ -250,7 +250,7 @@
     justify-content: center;
     padding: 5px 0;
     position: relative;
-    left: 1px; /* Pequeño ajuste para centrar en el sidebar colapsado */
+    /*left: 1px;  Pequeño ajuste para centrar en el sidebar colapsado */
 }
 
 /* Ocultar la estructura de columnas cuando está colapsado */
@@ -266,7 +266,7 @@
     margin: 0 auto;
     overflow: hidden;
     position: relative;
-    left: -1px; /* Pequeño ajuste para alinear con el diseño */
+    /*left: -1px; Pequeño ajuste para alinear con el diseño */
 }
 
 /* Ajuste para la imagen cuando el sidebar está colapsado */
@@ -290,7 +290,7 @@
     height: 40px;
     border-radius: 50%;
     position: relative;
-    left: -1px; /* Pequeño ajuste para alinear con el diseño */
+    /*left: -1px;  Pequeño ajuste para alinear con el diseño */
 }
 
 /* Tooltip para modo colapsado */
