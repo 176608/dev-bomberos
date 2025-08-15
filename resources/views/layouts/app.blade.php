deb6087fdb9540fcb283680323a2e2b8195becd8
176608 <al176608@alumnos.uacj.mx>
2025-08-15 12:28:42 -0600

    aver nuevo fondo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,7 +126,7 @@
 
     .bg-fonde {
         background-image: url('{{ asset('imagenes/fondo.png') }}');
-        background-size: contain;
+        background-size: cover;
         background-position: center;
         background-repeat: repeat-y;
     }
