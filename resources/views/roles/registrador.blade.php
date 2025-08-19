bc05456497c73ca8015ccfd158866e6d279361dc
176608 <al176608@alumnos.uacj.mx>
2025-08-18 21:24:02 -0600

    check padding bottom

--- resources/views/roles/registrador.blade.php
+++ resources/views/roles/registrador.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 <div class="container-fluid mt-4">
-    <div class="row">
+    <div class="row pb-4">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center mb-4">
                 <h2 class="text-primary">
