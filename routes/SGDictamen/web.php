99d9af1939fe73f956894ecd25d0008343543250
NotDerrida <alanedgar002@gmail.com>
2026-05-05 12:05:01 -0600

    Finalizacion de acceso a interfaz de dictamenes

--- routes/SGDictamen/web.php
+++ routes/SGDictamen/web.php
@@ -13,22 +13,22 @@ Route::middleware(['auth', \App\Http\Middleware\PreventBackHistory::class])
     ->name('sg-dictamen.')
     ->group(function () {
     
-    // Index: Listado - Acceso para Admin Dictamenes y Editores
+    // Index: Listado - Acceso para Admin Dictamenes, Editores y Desarrollador
     Route::get('/', [DictamenController::class, 'index'])->name('index')
-        ->middleware('role:Administrador Dictamenes,Editor Dictamenes');
+        ->middleware('role:Administrador Dictamenes,Editor Dictamenes,Desarrollador');
     
     // Ver detalle
     Route::get('/{dictamen}/view', [DictamenController::class, 'view'])->name('view')
-        ->middleware('role:Administrador Dictamenes,Editor Dictamenes');
+        ->middleware('role:Administrador Dictamenes,Editor Dictamenes,Desarrollador');
     
-    // Editar + Actualizar - Admin Dictamenes y Editores
-    Route::middleware('role:Administrador Dictamenes,Editor Dictamenes')->group(function () {
+    // Editar + Actualizar - Admin Dictamenes, Editores y Desarrollador
+    Route::middleware('role:Administrador Dictamenes,Editor Dictamenes,Desarrollador')->group(function () {
         Route::get('/{dictamen}/edit', [DictamenController::class, 'edit'])->name('edit');
         Route::put('/{dictamen}', [DictamenController::class, 'update'])->name('update');
     });
     
-    // Crear + Eliminar + Ver Eliminados - Solo Admin Dictamenes
-Route::middleware('role:Administrador Dictamenes')->group(function () {
+    // Crear + Eliminar + Ver Eliminados - Admin Dictamenes y Desarrollador
+Route::middleware('role:Administrador Dictamenes,Desarrollador')->group(function () {
     Route::post('/', [DictamenController::class, 'store'])->name('store');
     Route::delete('/{dictamen}', [DictamenController::class, 'destroy'])->name('destroy');
     
@@ -39,9 +39,9 @@ Route::middleware('role:Administrador Dictamenes')->group(function () {
 // Ruta para RESTAURAR un dictamen eliminado
 Route::post('/deleted/{id}/restore', [DictamenController::class, 'restoreDeleted'])
     ->name('restore')
-    ->middleware('role:Administrador Dictamenes');
+    ->middleware('role:Administrador Dictamenes,Desarrollador');
 
     // AJAX para DataTables
     Route::get('/data', [DictamenController::class, 'dataTable'])->name('data')
-        ->middleware('role:Administrador Dictamenes,Editor Dictamenes');
+        ->middleware('role:Administrador Dictamenes,Editor Dictamenes,Desarrollador');
 });
