c7483559e8241bf4d6ca51851a22fdc96b22c294
NotDerrida <alanedgar002@gmail.com>
2026-05-28 14:04:34 -0600

    Test icono en estadistica sigemv2

--- resources/views/VisorSIGEM/estadistica_tema.blade.php
+++ resources/views/VisorSIGEM/estadistica_tema.blade.php
@@ -360,6 +360,11 @@
                                                         {{ $indicador['codigo_cuadro'] ?? 'N/A' }}
                                                     </span>
                                                     {{ $indicador['cuadro_estadistico_titulo'] ?? 'Sin título' }}
+                                                    @if(!empty($indicador['tipo_mapa_pdf']))
+                                                        <span class="badge bg-warning text-dark ms-2">
+                                                            <i class="bi bi-map-fill me-1"></i>Mapa PDF
+                                                        </span>
+                                                    @endif
                                                 </span>
                                                 @if(!empty($indicador['cuadro_estadistico_subtitulo']))
                                                     <small class="text-muted d-block">{{ $indicador['cuadro_estadistico_subtitulo'] }}</small>
@@ -488,7 +493,11 @@ function renderizarIndicadores(indicadores) {
     ordenados.forEach(function (ind) {
         html += '<a href="javascript:void(0)" onclick="alert(\'ID del Indicador: ' + ind.cuadro_estadistico_id + '\')" class="cuadro-item p-3 mb-3 border rounded text-decoration-none d-block">';
         html += '<div class="row align-items-center"><div class="col-12">';
-        html += '<span class="mb-1 d-block text-dark"><span class="fw-bold text-success">' + (ind.codigo_cuadro || 'N/A') + '</span> ' + (ind.cuadro_estadistico_titulo || 'Sin título') + '</span>';
+        html += '<span class="mb-1 d-block text-dark"><span class="fw-bold text-success">' + (ind.codigo_cuadro || 'N/A') + '</span> ' + (ind.cuadro_estadistico_titulo || 'Sin título');
+        if (ind.tipo_mapa_pdf) {
+            html += ' <span class="badge bg-warning text-dark ms-2"><i class="bi bi-map-fill me-1"></i>Mapa PDF</span>';
+        }
+        html += '</span>';
         if (ind.cuadro_estadistico_subtitulo) {
             html += '<small class="text-muted d-block">' + ind.cuadro_estadistico_subtitulo + '</small>';
         }
