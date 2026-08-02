831c519417f697cbdb033cc90bfadf558f7c5ed0
NotDerrida <alanedgar002@gmail.com>
2026-08-02 16:13:21 -0600

    check

--- resources/views/gestor-dictamenes/index.blade.php
+++ resources/views/gestor-dictamenes/index.blade.php
@@ -109,8 +109,8 @@ tr:hover td {
     ];
 @endphp
 
-<div class="container-fluid p-0 bg-fonde" style="min-height: 100vh;">
-  <div class="container mt-4 pb-5">
+<div class="container-fluid mt-4 p-0 bg-fonde" style="min-height: 100vh;">
+  <div class="container pb-5">
 
     <!-- Estadísticas -->
     <div class="row mb-4" id="statsCards">
